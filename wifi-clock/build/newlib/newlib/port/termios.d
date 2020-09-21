ba7a86f6a64159209a5a0d1b43c97c3bd88b3c46
YHF <yhf_1404@outlook.com>
2020-09-21 16:46:49 +0800

    master

--- wifi-clock/build/newlib/newlib/port/termios.d
+++
@@ -1,6 +0,0 @@
-newlib/port/termios.o: \
- /home/yhf/esp/ESP8266_RTOS_SDK/components/newlib/newlib/port/termios.c \
- /home/yhf/my_esp8266_project/wulianwang/wifi-clock/build/include/sdkconfig.h
-
-/home/yhf/my_esp8266_project/wulianwang/wifi-clock/build/include/sdkconfig.h:
-/home/yhf/esp/ESP8266_RTOS_SDK/components/newlib/newlib/port/termios.c:
